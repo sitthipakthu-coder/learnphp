1b7bc0245fb52d022a74e188a134a5de0c644341
Sitthipak <sitthipak.thu@gmail.com>
2025-09-24 16:06:41 +0700

    4

---
+++ Compound_Assingment.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table,tr{
+            border: 1px solid;
+            color: black;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h4>Compound_Assingment</h4>
+
+    <table>
+        <tr>
+            <td>Assignment</td>
+            <td>รูปแบบการเขียน</td>
+            <td>ความหมาย</td>
+        </tr>
+        <tr>
+            <td>+=</td>
+            <td>x+=y</td>
+            <td>x=x+y</td>
+        </tr>
+        <tr>
+            <td>-=</td>
+            <td>x-=y</td>
+            <td>x=x-y</td>
+        </tr>
+        <tr>
+            <td>*=</td>
+            <td>x*=y</td>
+            <td>x=x*y</td>
+        </tr>
+        <tr>
+            <td>/=</td>
+            <td>x/=y</td>
+            <td>x=x/y</td>
+        </tr>
+        <tr>
+            <td>%=</td>
+            <td>x%=y</td>
+            <td>x=x%y</td>
+        </tr>
+    </table>
+    <?php
+
+    ?>
+</body>
+
+</html>
