7fc0b780dbce47d39fffffb5ec36f2315c1fa5d1
Stang <sitthipak.thu@gmail.com>
2026-04-09 12:55:55 +0700

    update login page09

--- Type_Casting.php
+++ Type_Casting.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-            <style>
-        p{
-            margin:0;
+    <style>
+        p {
+            margin: 0;
         }
     </style>
 </head>
+
 <body>
 
     <b>Type Casting</b>
     <p>คือ การเปลี่ยนชนิดของข้อมูลให้เป็นชนิดที่ต้องการโดยใส่ชนิดข้อมูลของตัวแปรไว้ในวงเล็บหน้าตัวแปรที่ต้องการจะเปลี่ยนชนิดของข้อมูบ</p>
     <p>---------------------------------------------------------------------------------------</p>
-<?php
+    <?php
 
     $price = "1000.20";
-        echo "ก่อนเปลี่ยน ".gettype($price)."<br>";
-        // settype($price,"int");
+    echo "ก่อนเปลี่ยน " . gettype($price) . "<br>";
+    // settype($price,"int");
     $dalivery =  50.70;
 
-     $total = ($price + $dalivery);
+    $total = ($price + $dalivery);
     echo "รวมราคา $total<br >";
-    echo gettype (" d $total ")."<br>";
-    settype($total,"int");
-    echo $total."<br>";
-    echo gettype ($total);
-    
-?>
-    
+    echo gettype(" d $total ") . "<br>";
+    settype($total, "int");
+    echo $total . "<br>";
+    echo gettype($total);
+
+    ?>
+
 </body>
+
 </html>
--- getandsettype.php
+++ getandsettype.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-        <style>
-        p{
-            margin:0;
+    <style>
+        p {
+            margin: 0;
         }
     </style>
 </head>
+
 <body>
     <p>แสดงชนิดข้อมูลของตัวแปร</p>
     <?php
@@ -20,17 +22,18 @@
     $nonisvaild = false; //Boolean
 
 
-        echo gettype($score)."<br>";
-        echo gettype($price)."<br>";
-        echo gettype($name)."<br>";
-        echo gettype($isvaild)."<br>";
-        echo gettype($nonisvaild)."<br>";
+    echo gettype($score) . "<br>";
+    echo gettype($price) . "<br>";
+    echo gettype($name) . "<br>";
+    echo gettype($isvaild) . "<br>";
+    echo gettype($nonisvaild) . "<br>";
 
-        echo "<p>---------------------------------------------------------------------------------------</p>";
-        settype($score,"Integer");
-        echo gettype($score)."<br>";
-        echo $score;
-    
+    echo "<p>---------------------------------------------------------------------------------------</p>";
+    settype($score, "Integer");
+    echo gettype($score) . "<br>";
+    echo $score;
+
     ?>
 </body>
+
 </html>
